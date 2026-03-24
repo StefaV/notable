0f14f3292e6ec098684dcf74087e59ef089da232
StefaV <stefav.99@gmail.com>
2026-03-24 22:36:22 +0100

    Note buttons added, save functionality auto updates note button list,

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.IO;
 using System.Diagnostics.Eventing.Reader;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Notes;
 
@@ -30,28 +31,58 @@ public partial class MainWindow : Window
 {
     public void Save(object sender, RoutedEventArgs e)
     {
-        string NotePath = @"Books\NovaBeleska.json";
         var saveInfo = new NoteInfo
         {
             Name = NoteTitle.Text,
             Content = NoteContent.Text
         };
 
-        string Note = JsonSerializer.Serialize(saveInfo);
+        var NotePath = $@"Books\{NoteTitle.Text}.json";
+        var Note = JsonSerializer.Serialize(saveInfo);
         File.WriteAllText(NotePath, Note);
+
+        Button NoteButton = new Button();
+        NoteButton.Tag = NotePath;
+        NoteButton.Content = NoteTitle.Text;
+        NoteButton.Height = 50;
+        NoteButton.BorderThickness = new Thickness(0);
+        NoteButton.Click += new RoutedEventHandler(Open);
+        NoteList.Children.Add(NoteButton);
 
     }
     public void Open(object sender, RoutedEventArgs e)
     {   
-        string FileName = @"Books\NovaBeleska.json";
-        string JsonString = File.ReadAllText(FileName);
+        Button ClickedButton = (Button)sender;
+
+        var FileName = $@"{ClickedButton.Tag}";
+        var JsonString = File.ReadAllText(FileName);
         NoteInfo Info = JsonSerializer.Deserialize<NoteInfo>(JsonString)!;
         
         NoteTitle.Text = Info.Name;
         NoteContent.Text = Info.Content;
     }
+
+    public void ListNotes()
+    {
+        IEnumerable<string> files = Directory.EnumerateFiles(@"C:\Users\Stefan\Desktop\Projekti\C#\Notes\Books","*.json");
+
+        foreach (var file in files)
+        {
+            var JsonString = File.ReadAllText(file);
+            NoteInfo Info = JsonSerializer.Deserialize<NoteInfo>(JsonString)!;
+
+            Button NoteButton = new Button();
+            NoteButton.Tag = file;
+            NoteButton.Content = Info.Name;
+            NoteButton.Height = 50;
+            NoteButton.BorderThickness = new Thickness(0);
+            NoteButton.Click += new RoutedEventHandler(Open);
+            NoteList.Children.Add(NoteButton);
+        }
+    }
     public MainWindow()
     {
         InitializeComponent();
+        ListNotes();
     }
 }
